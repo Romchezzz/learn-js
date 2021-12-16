07731404721972d1422c6d8e771ee158e6e626ef
Romchezzz <forevv@gmail.com>
2021-12-16 11:20:34 +0200

    1

--- answers/files/1.48/script.js
+++ answers/files/1.48/script.js
@@ -1,19 +1,66 @@
+let leftOperand;
+let rightOperand;
+let result;
+let operator;
+let expression;
+
 function onLeftOperandChange(value) {
-  console.log('onLeftOperandChange', value)
+  leftOperand = +value;
 }
 
 function onRightOperandChange(value) {
-  console.log('onRightOperandChange', value)
+  rightOperand = +value;
 }
 
 function onOperatorChange(value) {
-  console.log('onOperatorChange', value)
+  operator = value;
 }
 
 function onSubmit () {
-  console.log('onSubmit')
+  switch (operator) {
+    case'ADDITION':
+      expression =  (`(${expression || leftOperand} + ${rightOperand})`);
+      result = leftOperand + rightOperand;
+      break;
+
+    case'SUBTRACTION':
+      expression = (`(${expression || leftOperand} - ${rightOperand})`);
+      result = leftOperand - rightOperand
+       break;
+
+    case'MULTIPLICATION':
+      expression = (`(${expression || leftOperand} * ${rightOperand})`);
+      result = leftOperand * rightOperand
+      break;
+
+    case'EXPONENTIATION':
+      expression = (`(${expression || leftOperand}xª ${rightOperand})`);
+      result = leftOperand ** rightOperand
+      break;
+
+    case'DIVISION':
+      expression = (`(${expression || leftOperand}/ ${rightOperand})`);
+      result = leftOperand / rightOperand
+      break;
+
+    case'MODULUS':
+      expression = (`(${expression || leftOperand}% ${rightOperand})`);
+      result = leftOperand % rightOperand
+      break;
+
+    case'ROOT':
+      expression = (`(${expression || leftOperand}√ ${rightOperand})`);
+      result = rightOperand ** (1/leftOperand)
+      break;
+
+  }
+
+  setExpression(`${expression} = ${result}`);
+  setResult(result)
 }
 
 function onClear () {
-  console.log('onClear')
+  expression = '';
 }
+
+
