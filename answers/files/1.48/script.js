c28418e43b27d6a5c23c8fb7b9f1ffc842dbb3ef
Romchezzz <forevv@gmail.com>
2021-12-19 15:28:59 +0200

    2.4

--- answers/files/1.48/script.js
+++ answers/files/1.48/script.js
@@ -4,6 +4,7 @@ let result;
 let operator;
 let expression;
 
+
 function onLeftOperandChange(value) {
   leftOperand = +value;
 }
@@ -17,46 +18,47 @@ function onOperatorChange(value) {
 }
 
 function onSubmit () {
+    let previousExp = expression || leftOperand;
   switch (operator) {
-    case'ADDITION':
-      expression =  (`(${expression || leftOperand} + ${rightOperand})`);
+    case 'ADDITION':
+      expression =  (`(${previousExp} + ${rightOperand})`);
       result = leftOperand + rightOperand;
       break;
 
-    case'SUBTRACTION':
-      expression = (`(${expression || leftOperand} - ${rightOperand})`);
+    case 'SUBTRACTION':
+      expression = (`(${previousExp} - ${rightOperand})`);
       result = leftOperand - rightOperand;
        break;
 
-    case'MULTIPLICATION':
-      expression = (`(${expression || leftOperand} * ${rightOperand})`);
+    case 'MULTIPLICATION':
+      expression = (`(${previousExp} * ${rightOperand})`);
       result = leftOperand * rightOperand;
       break;
 
-    case'EXPONENTIATION':
-      expression = (`(${expression || leftOperand}xª ${rightOperand})`);
+    case 'EXPONENTIATION':
+      expression = (`(${previousExp}xª ${rightOperand})`);
       result = leftOperand ** rightOperand;
       break;
 
-    case'DIVISION':
-      expression = (`(${expression || leftOperand}/ ${rightOperand})`);
+    case 'DIVISION':
+      expression = (`(${previousExp}/ ${rightOperand})`);
       result = leftOperand / rightOperand;
       break;
 
-    case'MODULUS':
-      expression = (`(${expression || leftOperand}% ${rightOperand})`);
+    case 'MODULUS':
+      expression = (`(${previousExp}% ${rightOperand})`);
       result = leftOperand % rightOperand;
       break;
 
-    case'ROOT':
-      expression = (`(${expression || leftOperand}√ ${rightOperand})`);
+    case 'ROOT':
+      expression = (`(${previousExp}√ ${rightOperand})`);
       result = rightOperand ** (1/leftOperand);
       break;
 
   }
 
-  setExpression(`${expression} = ${result}` && '');
-  setResult(result);
+  setExpression (`${expression} = ${result}`);
+  setResult (result);
 }
 
 function onClear () {
